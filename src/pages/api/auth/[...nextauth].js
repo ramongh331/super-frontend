84f8d867950b9f5f00acaa5cd0ed303af856ea3c
Ramon Hernandez <ramongh331@gmail.com>
2023-03-13 23:46:43 -0400

    uninstalled mongodb-adapter. I don't think I need it.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 
 import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GoogleProvider from 'next-auth/providers/google'
-import clientPromise from "../../../../lib/mongodb";
+// import clientPromise from "../../../../lib/mongodb";
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
