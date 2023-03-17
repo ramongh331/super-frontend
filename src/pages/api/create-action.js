3d1b1cb1a29c1199d3361f9725c4a747acbf66ae
Ramon Hernandez <ramongh331@gmail.com>
2023-03-17 18:58:14 -0400

    updated redirect after submission to the profile getting created

--- src/pages/api/create-action.js
+++ src/pages/api/create-action.js
@@ -1,28 +1,31 @@
-
-import { MongoClient } from 'mongodb';
-import { getSession } from 'next-auth/react';
+import { MongoClient } from "mongodb";
+import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      const formData = req.body;
-  
-      // Retrieve the user's session to get their email
-      const session = await getSession({ req });
-      const userEmail = session?.user?.email ?? null;
-  
-      // Connect to MongoDB instance
-      const client = await MongoClient.connect(process.env.MONGODB_URI);
-      const db = client.db(process.env.MONGODB_DB);
-  
-      // Insert the form data into the "todos" collection with the user's email
-      await db.collection('profile').insertOne({
-        ...formData,
-        userEmail
-      });
-  
-      client.close();
-      res.redirect('/connections');
-    } else {
-      res.status(405).json({ message: 'Method not allowed' });
-    }
+  if (req.method === "POST") {
+    const formData = req.body;
+
+    // Retrieve the user's session to get their email
+    const session = await getSession({ req });
+    const userEmail = session?.user?.email ?? null;
+
+    // Connect to MongoDB instance
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db(process.env.MONGODB_DB);
+
+    // Insert the form data into the "todos" collection with the user's email
+    await db.collection("profile").insertOne({
+      ...formData,
+      userEmail,
+    });
+
+    // get specific profile of user signed in
+    const profile = await db.collection("profile").findOne({ userEmail });
+    const serializedProfile = JSON.parse(JSON.stringify(profile));
+
+    client.close();
+    res.redirect(`/profile/view/${serializedProfile._id}`);
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
   }
+}
