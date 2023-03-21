ab98361dae6ade344d301329537e420af61f0d53
Ramon Hernandez <ramongh331@gmail.com>
2023-03-20 22:19:20 -0400

    Removed security logic to redirect pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,17 @@
 import Head from "next/head";
-import { signIn, signOut, getSession, useSession } from "next-auth/react";
-import { MongoClient } from "mongodb";
-import { useEffect, useState } from "react";
+import { signIn, signOut, getSession } from "next-auth/react";
 
 export async function getServerSideProps(context) {
-  console.log('server side props have rendered')
   const session = await getSession(context);
-  const userEmail = session?.user?.email;
 
-  // CONNECTING TO MONGO DATABASE
-  const client = await MongoClient.connect(process.env.MONGODB_URI);
-  const db = client.db(process.env.MONGODB_DB);
-
-  // REFERRING TO PRFOILE COLLECTION BASED ON THE CURRENT USER'S EMAIL
-  const profileData = await db.collection("profile").findOne({ userEmail });
-  
-  client.close();
-
-  // CONVERTING THE DATA INTO READABLE JSON
-  const serializedData = JSON.parse(JSON.stringify(profileData));
-
-  // RETURNING THE DATA AS PROPS
   return {
     props: {
-      profileData: serializedData,
+      session,
     },
   };
 }
 
-export default function Home({profileData}) {
-  const {data: session } = useSession()
-// THE REDIRECT URL IS SET DEPENDING ON THE CONDITION
-  const [redirectUrl, setRedirectUrl ] = useState()
-
-  // I THOUGHT THIS SHOULD HAPPEN AFTER SESSION DATA IS RECEIVED BUT IT ISN'T
-  useEffect(() => {
-    console.log('useEffect is running')
-    if (profileData?.userEmail === session?.user?.email){
-      setRedirectUrl('/connections')
-      console.log('It was true')
-    } else {
-      setRedirectUrl('/profile/new')
-      console.log('It was false')
-    }
-  }, [session])
+export default function Home({ session}) {
 
   if (session) {
     return (
@@ -74,7 +42,7 @@ export default function Home({profileData}) {
         </Head>
         <main>
           <h2>Home Page</h2>
-          <button onClick={() => signIn('google', {callbackUrl: redirectUrl})}>Sign In</button>
+          <button onClick={() => signIn('google', {callbackUrl: '/connections'})}>Sign In</button>
         </main>
       </>
     );
