08574ee3747a7edffe05d37b5c75b998f8f31d40
Ramon Hernandez <ramongh331@gmail.com>
2023-03-22 19:57:06 -0400

    created a sign out version of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,12 +23,52 @@ export default function Home({ session}) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main>
-          <h2>Home Page</h2>
-          <button onClick={() => signOut()}>Sign Out</button>
-          <h3>Profile Info</h3>
-          <p>Signed in as: {session.user.name}</p>
-          <p>Email: {session.user.email}</p>
+        <header className="w-11/12 h-36 mx-auto flex items-center justify-between ">
+          <div className="flex items-center justify-between w-48">
+            <img className="w-16" src="https://i.imgur.com/0BIn6HA.png" alt="Super Logo"/>
+            <h1 className="text-4xl text-[#4478ff] font-bold">SUPER</h1>
+            </div>
+            <section className="flex w-[55rem] justify-between text-xl">
+              <h2>Why Super?</h2>
+              <h2>How does it work?</h2>
+              <h2>Pricing</h2>
+              <h2>Contact</h2>
+            </section>
+            <section>
+              <motion.button 
+              whileHover={{scale: 1.2}} 
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              className="text-xl font-semibold border-2 border-[#4478ff] rounded-md px-2" 
+              onClick={() => signOut({callbackUrl: '/'})}>Sign Out</motion.button>
+            </section>
+        </header>
+        <main className="w-full h-[66.7vh]">
+          <section className="w-fit mx-auto mt-5">
+            <h3 className="text-5xl">Join <span className="">Super</span> and...</h3>
+            <h3 className="text-8xl font-semibold">Find <span className="text-[#4478ff]">Your</span> Super <span className="text-[#4478ff]">Match</span></h3>
+          </section>
+           <section className="w-full flex justify-evenly mt-10 relative" >
+            <motion.img 
+            src="https://i.imgur.com/KklYWBT.png" className="w-[290px] object-contain" alt="Wizard Card"
+            animate={{y: [28, -50, 28]}}
+            transition={{duration: 5, repeat: Infinity, ease: "easeInOut", delay: 0.9}}
+            />
+            <motion.img 
+            src="https://i.imgur.com/FykyQB3.png" className="w-[290px] object-contain" alt="Sarg Card"
+            animate={{y: [28, -50, 28]}}
+            transition={{duration: 5, repeat: Infinity, ease: "easeInOut"}}
+            />
+            <motion.img 
+            src="https://i.imgur.com/4ML1GiN.png" className="w-[290px] object-contain right-[40%]" alt="Wonder Card"
+            animate={{y: [28, -50, 28]}}
+            transition={{duration: 5, repeat: Infinity, ease: "easeInOut", delay: 0.3}}
+            />
+            <motion.img 
+            src="https://i.imgur.com/Evhsjgw.png" className="w-[390px] object-contain right-[25%]" alt="Joker Card"
+            animate={{y: [32, -50, 32]}}
+            transition={{duration: 5, repeat: Infinity, ease: "easeInOut"}}
+            />
+            </section>
         </main>
       </>
     );
@@ -53,7 +93,11 @@ export default function Home({ session}) {
               <h2>Contact</h2>
             </section>
             <section>
-              <button className="text-xl" onClick={() => signIn('google', {callbackUrl: '/connections'})}>Sign In</button>
+              <motion.button 
+              whileHover={{scale: 1.2}} 
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              className="text-xl font-semibold border-2 border-[#4478ff] rounded-md px-2" 
+              onClick={() => signIn('google', {callbackUrl: '/connections'})}>Sign In</motion.button>
             </section>
         </header>
         <main className="w-full h-[66.7vh]">
