84c92c7795454c252aeca6dd4150282b557214bb
Ramon Hernandez <ramongh331@gmail.com>
2023-03-25 14:19:12 -0400

    styled new page

--- src/pages/profile/new.js
+++ src/pages/profile/new.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import { motion } from "framer-motion";
 import { signOut, signIn, getSession } from "next-auth/react";
 
 export async function getServerSideProps(context) {
@@ -30,105 +32,144 @@ export default function New({ session }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main>
-          <button onClick={() => signOut()}>Sign Out</button>
-          <h2>New Profile Page</h2>
-          <p>{session.user.name}</p>
-          <p>{session.user.email}</p>
-          <form action="/api/create-action" method="POST" className="flex flex-col">
-            <h3>Identity</h3>
+        <header className="w-11/12 h-[8vh] mx-auto flex items-center justify-between ">
+          <Link href="/">
+            <div className="flex items-center justify-between w-48">
+              <img className="w-16" src="https://i.imgur.com/0BIn6HA.png" alt="Super Logo"/>
+              <h1 className="text-4xl text-[#4478ff] font-bold">SUPER</h1>
+              </div>
+          </Link>
             <section>
-              <label>Super Name:</label>
-              <input className="border-solid border-2 border-black" type="text" name="sname" placeholder="Super Name" />
+              <motion.button 
+              whileHover={{scale: 1.2}} 
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              className="text-xl font-semibold border-2 border-[#4478ff] rounded-md px-2" 
+              onClick={() => signOut({callbackUrl: '/'})}>Sign Out</motion.button>
             </section>
-            <section>
-              <label>Real Name:</label>
-              <input className="border-solid border-2 border-black" type="text" name="rname" placeholder="Real Name" />
+        </header>
+        <main className="w-full h-full bg-blue-100">
+          <h2 className="text-center text-5xl py-10">Set up profile</h2>
+
+          <form action="/api/create-action" method="POST" className="w-full h-full">
+            <section className="w-fit h-full mx-auto"> 
+              {/* Card 1 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}} 
+              className="w-[60rem] h-[30rem] flex flex-col items-center justify-center bg-white rounded-3xl mb-72 shadow-lg">
+                <label className="text-3xl mb-2">Super Name</label>
+                <input className="border-solid border-2 border-black text-xl p-2 rounded-lg" type="text" name="sname" placeholder="Super Name" />
+              </motion.section>
+              
+              {/* Card 2 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}}
+              className="w-[60rem] h-[30rem] flex items-center justify-evenly bg-white rounded-3xl mb-72 shadow-lg">
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Real Name</label>
+                  <input className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" type="text" name="rname" placeholder="Real Name" />
+                </section>
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Age</label>
+                  <input className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" type="number" min="0" name="age" placeholder="Age" />
+                </section>
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Location</label>
+                  <input className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" type="text" name="location" placeholder="Location" />
+                </section>
+              </motion.section>
+
+                {/* Card 3 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}} 
+              className="w-[60rem] h-[30rem] flex items-center justify-evenly bg-white rounded-3xl mb-72 shadow-lg">  
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Species</label>
+                  <input className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" type="text" name="species" placeholder="Species" />
+                </section>
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Gender (if human)</label>
+                  <select defaultValue={'DEFAULT'} className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" name="gender">
+                    <option value="DEFAULT" disabled hidden>Gender</option>
+                    <option value="Agender">Agender</option>
+                    <option value="Androgynous">Androgynous</option>
+                    <option value="Bigender">Bigender</option>
+                    <option value="Cisgender">Cisgender</option>
+                    <option value="Cis Woman">Cis Woman</option>
+                    <option value="Cis Man">Cis Man</option>
+                    <option value="Non-binary">Non-binary</option>
+                    <option value="Gender Fluid">Gender Fluid</option>
+                    <option value="Gender Questioning">Gender Questioning</option>
+                    <option value="Transgender">Transgender</option>
+                    <option value="Trans Woman">Trans Woman</option>
+                    <option value="Trans Man">Trans Man</option>
+                    <option value="Transgender Person">Transgender Person</option>
+                    <option value="Two-Spirit">Two-Spirit</option>
+                  </select>
+                </section>
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Sexual Orientation</label>
+                  <select defaultValue={'DEFAULT'} className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" name="sex">
+                    <option value="DEFAULT" disabled hidden>Sexual Orientation</option>
+                    <option value="Asexual">Asexual</option>
+                    <option value="Bisexual">Bisexual</option>
+                    <option value="Gay">Gay</option>
+                    <option value="Lesbian">Lesbian</option>
+                    <option value="Pansexual">Pansexual</option>
+                    <option value="Polysexual">Polysexual</option>
+                    <option value="Queer">Queer</option>
+                    <option value="Questioning">Questioning</option>
+                    <option value="Straight">Straight</option>
+                  </select>
+              </section>
+              </motion.section>
+
+              {/* Card 4 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}}
+              className="w-[60rem] h-[30rem] flex flex-col items-center justify-center bg-white rounded-3xl mb-72 shadow-lg">
+                <label className="text-3xl mb-2">Abilities</label>
+                <input className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" type="text" name="ability" placeholder="Abilities" />
+              </motion.section>
+              
+              {/* Card 5 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}}
+              className="w-[60rem] h-[30rem] flex items-center justify-evenly bg-white rounded-3xl mb-72 shadow-lg">
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Pick your side</label>
+                  <select  defaultValue={'DEFAULT'} className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" name="hva">
+                    <option value="DEFAULT" disabled hidden>Select your Side</option>
+                    <option value="Hero">Hero</option>
+                    <option value="Villain">Villain</option>
+                    <option value="Anti-Hero">Anti-Hero</option>
+                  </select>
+                </section>
+                <section className="flex flex-col text-center">
+                  <label className="text-3xl mb-2">Affiliation</label>
+                  <select defaultValue={'DEFAULT'} className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4" name="tpi">
+                    <option value="DEFAULT" disabled hidden>Team/Partner/Indie</option>
+                    <option value="I am on a team">I&#39;m on a team</option>
+                    <option value="I have a partner">I have a partner</option>
+                    <option value="I work independently">I work independently</option>
+                  </select>
+              </section>
+              </motion.section>
+              
+              {/* Card 6 */}
+              <motion.section initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once:true}}
+              id="card6" 
+              className="w-[60rem] h-[30rem] flex flex-col items-center justify-center bg-white rounded-3xl shadow-lg">
+                <label className="text-3xl mb-2">Backstory</label>
+                <textarea
+                className="border-solid border-2 border-black text-xl p-2 rounded-lg mb-4"
+                  name="story"
+                  rows="4"
+                  cols="50"
+                  maxLength="500"
+                  placeholder="Born with abilities beyond imagination, I grew up in a world that could not understand them..."
+                />
+              <motion.button 
+              whileHover={{scale: 1.1}} 
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              className="w-52 p-2 rounded-lg font-semibold bg-[#4478ff] text-white text-xl" type="submit">Create Super Profile</motion.button>
+              </motion.section>
             </section>
-            <section>
-              <label>Age:</label>
-              <input className="border-solid border-2 border-black" type="number" min="0" name="age" placeholder="Age" />
-            </section>
-            <section>
-              <label>Species:</label>
-              <input className="border-solid border-2 border-black" type="text" name="species" placeholder="Species" />
-            </section>
-            <section>
-              <label>Gender (if human):</label>
-              <select defaultValue={'DEFAULT'} className="border-solid border-2 border-black" name="gender">
-                <option value="DEFAULT" disabled hidden>Gender</option>
-                <option value="Agender">Agender</option>
-                <option value="Androgynous">Androgynous</option>
-                <option value="Bigender">Bigender</option>
-                <option value="Cisgender">Cisgender</option>
-                <option value="Cis Woman">Cis Woman</option>
-                <option value="Cis Man">Cis Man</option>
-                <option value="Non-binary">Non-binary</option>
-                <option value="Gender Fluid">Gender Fluid</option>
-                <option value="Gender Questioning">Gender Questioning</option>
-                <option value="Transgender">Transgender</option>
-                <option value="Trans Woman">Trans Woman</option>
-                <option value="Trans Man">Trans Man</option>
-                <option value="Transgender Person">Transgender Person</option>
-                <option value="Two-Spirit">Two-Spirit</option>
-              </select>
-            </section>
-            <section>
-              <label>Sexual Orientation:</label>
-              <select defaultValue={'DEFAULT'} className="border-solid border-2 border-black" name="sex">
-                <option value="DEFAULT" disabled hidden>Sexual Orientation</option>
-                <option value="Asexual">Asexual</option>
-                <option value="Bisexual">Bisexual</option>
-                <option value="Gay">Gay</option>
-                <option value="Lesbian">Lesbian</option>
-                <option value="Pansexual">Pansexual</option>
-                <option value="Polysexual">Polysexual</option>
-                <option value="Queer">Queer</option>
-                <option value="Questioning">Questioning</option>
-                <option value="Straight">Straight</option>
-              </select>
-            </section>
-            <section>
-              <label>Location:</label>
-              <input className="border-solid border-2 border-black" type="text" name="location" placeholder="Location" />
-            </section>
-            
-            <h3>Attributes</h3>
-            <section>
-              <label>Abilities:</label>
-              <input className="border-solid border-2 border-black" type="text" name="ability" placeholder="Abilities" />
-            </section>
-            <section>
-              <label>Pick your side:</label>
-              <select  defaultValue={'DEFAULT'} className="border-solid border-2 border-black" name="hva">
-                <option value="DEFAULT" disabled hidden>Select your Side</option>
-                <option value="Hero">Hero</option>
-                <option value="Villain">Villain</option>
-                <option value="Anti-Hero">Anti-Hero</option>
-              </select>
-            </section>
-            <section>
-              <label>Affiliation:</label>
-              <select defaultValue={'DEFAULT'}className="border-solid border-2 border-black" name="tpi">
-                <option value="DEFAULT" disabled hidden>Team/Partner/Indie</option>
-                <option value="I am on a team">I&#39;m on a team</option>
-                <option value="I have a partner">I have a partner</option>
-                <option value="I work independently">I work independently</option>
-              </select>
-            </section>
-            <h3>Bio</h3>
-            <section>
-              <label>Backstory:</label>
-              <textarea
-              className="border-solid border-2 border-black"
-                name="story"
-                rows="4"
-                cols="50"
-                maxLength="500"
-                placeholder="Born with abilities beyond imagination, I grew up in a world that could not understand them..."
-              />
-            </section>
-            <button className="bg-gray-300 w-[180px]" type="submit">Create Super Profile</button>
+            <div className="w-full h-72"></div>
           </form>
         </main>
       </>
